53519dd46afa830bee4e24b65f4aecd0db20fe64
dhevadayatvito.ddv <dhevadayatvito.ddv@gmail.com>
2024-12-02 16:32:32 +0700

    update redis config

--- src/main/java/com/enigma/tokonyadia_api/config/RedisConfig.java
+++ src/main/java/com/enigma/tokonyadia_api/config/RedisConfig.java
@@ -1,21 +1,47 @@
 package com.enigma.tokonyadia_api.config;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.net.URI;
 
 @Configuration
 public class RedisConfig {
 
+//    @Bean
+//    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, String> template = new RedisTemplate<>();
+//        template.setConnectionFactory(connectionFactory);
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new StringRedisSerializer());
+//        return template;
+//    }
+
+    @Value("${REDIS_URL}")
+    private String redisUrl;
+
     @Bean
-    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
+    public LettuceConnectionFactory redisConnectionFactory() {
+        URI uri = URI.create(redisUrl);
+
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(uri.getHost());
+        redisConfig.setPort(uri.getPort());
+
+        // Extract password if available
+        if (uri.getUserInfo() != null) {
+            String password = uri.getUserInfo().split(":", 2)[1];
+            redisConfig.setPassword(password);
+        }
+
+        return new LettuceConnectionFactory(redisConfig);
     }
 
 }
